913fabc3885d114e0f8c06a376c8815c3c7ad438
Andrea Chagas <andrea.lima.chagas@gmail.com>
2019-03-29 08:08:28 +0800

    Added product control and changed manager to add control

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './products.dart';
+import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
@@ -33,21 +34,19 @@ class _ProductManagerState extends State<ProductManager> {
     super.didUpdateWidget(oldWidget);
   }
 
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print('[ProductManager State] build().');
     return Column(children: [
       Container(
-        margin: EdgeInsets.all(10.0),
-        child: RaisedButton(
-          color: Theme.of(context).primaryColor,
-          onPressed: () {
-            setState(() {
-              _products.add('Advanced Food Tester');
-            });
-          },
-          child: Text('Add Product'),
-        ),
+        margin: EdgeInsets.all(8.0),
+        child: ProductControl(_addProduct),
       ),
     Products(_products)  
     ],);
